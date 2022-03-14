bef5652246a68efcf5476c6b61d5eb119b2d78aa
nahla <nahlat91@gmail.com>
2022-03-14 04:28:19 +0200

    Bloc concepts

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutterly_bloc/cubit/counter_states.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState(counterValue: 0));
+
+  void increment()=> emit(CounterState(counterValue: state.counterValue+1));
+  void decrement()=> emit(CounterState(counterValue: state.counterValue-1));
+}
---
+++ lib/cubit/counter_states.dart
@@ -0,0 +1,7 @@
+
+class CounterState{
+  int counterValue;
+  bool? wasIncremented = true;
+
+  CounterState({required this.counterValue,  this.wasIncremented});
+}
